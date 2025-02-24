724368989afaa3d6d1e1b921cfc38042ed6e810b
Felipe Savaris <fesavaris@gmail.com>
2025-02-24 16:42:46 -0300

    feat: adds integrity handling to the POST users endpoint

--- libraries/adapters/outbound/db/user_repository_impl.py
+++ libraries/adapters/outbound/db/user_repository_impl.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import IntegrityError
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
@@ -7,12 +8,21 @@ from libraries.domain.entities.models import User
 from libraries.adapters.outbound.db.session import get_db
 
 
+class UserAlreadyExistsError(Exception): ...
+
 class UserRepositoryImpl(UserRepository):
     def create_user(self, user: UserInDTO) -> UserOutDTO:
         db: Session = next(get_db())
         db_user = User(**user.model_dump())
-        db.add(db_user)
-        db.commit()
-        db.refresh(db_user)
         
+        try:
+            db.add(db_user)
+            db.commit()
+            db.refresh(db_user)
+        except IntegrityError as err:
+            db.rollback()
+            if "users.name" in str(err) or "users.email" in str(err):
+                raise UserAlreadyExistsError("Nome de usuário ou email já cadastrado.") from err
+            raise
+           
         return UserOutDTO.model_validate(db_user.__dict__)
--- libraries/domain/use_cases.py
+++ libraries/domain/use_cases.py
@@ -1,3 +1,6 @@
+from fastapi import HTTPException, status
+
+from libraries.adapters.outbound.db.user_repository_impl import UserAlreadyExistsError
 from libraries.domain.entities.dtos import UserInDTO, UserOutDTO
 from libraries.domain.repositories import UserRepository
 
@@ -7,4 +10,7 @@ class CreateUserUseCase:
         self.user_repository = user_repository
 
     def execute(self, user: UserInDTO) -> UserOutDTO:
-        return self.user_repository.create_user(user)
+        try:
+            return self.user_repository.create_user(user)
+        except UserAlreadyExistsError as err:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err)) from err
