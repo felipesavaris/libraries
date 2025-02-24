fecbc1daf10122801673c45db21b4f128a0f25e2
Felipe Savaris <fesavaris@gmail.com>
2025-02-24 16:39:27 -0300

    feat: adds exception handling to database session

--- libraries/adapters/outbound/db/session.py
+++ libraries/adapters/outbound/db/session.py
@@ -6,9 +6,14 @@ from libraries.config import config_database
 engine = create_engine(config_database.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()
+        try:
+            db.close()
+        except Exception as err:
+            print(f'Error closing session: {err}')
+            pass
